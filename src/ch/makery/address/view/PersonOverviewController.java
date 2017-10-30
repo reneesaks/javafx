7d0c71c5ef873d3632f61268463c3762f51c6a76
reneesaks <renee.saks@khk.ee>
2017-10-30 14:18:56 +0200

    The Delete Button.

--- src/ch/makery/address/view/PersonOverviewController.java
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -96,4 +96,13 @@ public class PersonOverviewController {
             birthdayLabel.setText("");
         }
     }
+
+    /**
+     * Called when the user clicks on the delete button.
+     */
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+        personTable.getItems().remove(selectedIndex);
+    }
 }
